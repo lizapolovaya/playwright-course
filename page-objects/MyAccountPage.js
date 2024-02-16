e635116d0b9785982541da121c0b5cdd6c506403
Liza <liza.polovaya@gmail.com>
2024-02-16 17:04:56 +0200

    Logining with injected token

--- page-objects/MyAccountPage.js
+++ page-objects/MyAccountPage.js
@@ -1,10 +1,22 @@
+import { expect } from "@playwright/test"
+
 export class MyAccountPage {
+     /**
+     * @param {import("@playwright/test").Page} page 
+     */
+
     constructor(page) {
         this.page = page
+
+        this.pageHeading = page.getByRole('heading', {name:'My Account'})
     }
 
     async visit() {
         await this.page.goto("/my-account")
         await this.page.pause()
+    }
+
+    async waitForPageHeading() {
+        await this.pageHeading.waitFor()
     }
 }
--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,9 +1,17 @@
 import { test } from "@playwright/test"
 import { MyAccountPage } from "./../page-objects/MyAccountPage"
+import { getLoginToken } from "../api-calls/getLoginToken" 
 
 test.only("My account using cookie injection", async ({ page }) => {
+    const loginToken = await getLoginToken()
+    console.warn({loginToken})
+    
     const myAccount = new MyAccountPage(page)
     await myAccount.visit()
-
+    await page.evaluate(([loginTokenInsideBrowserCode]) => {
+        document.cookie = "token=" + loginTokenInsideBrowserCode
+    }, [loginToken])
+    await myAccount.visit()
+    await myAccount.waitForPageHeading()
 
 })
